0cba68b6bd14c63123d9ab7541e4d4dbbd9cbc7a
developer <developer@localhost>
2019-09-26 00:48:41 +0700

    Change data to Eng.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,22 +1,22 @@
 
 insert into category (id, name)
-values (1, 'Sport');
+values (1, 'IT');
 
 insert into category (id, name)
-values (2, 'Cooking');
+values (2, 'Toy');
 
 
 insert into product (id, name, description, price, category_id)
-values (1, 'เบรคก้ามปู','เบรคจักรยานก้ามปูแบบธรรมดา', 50.00, 1);
+values (1, 'Wireless Mouse','Wireless Mouse', 50.00, 1);
 
 insert into product (id, name, description, price, category_id)
-values (2, 'อานจักรยานแม่บ้าน','อานจักรยานแม่บ้าน ดำ เบาะหนา', 100.00, 1);
-
-
-insert into product (id, name, description, price, category_id)
-values (3, 'ยางในจักรยาน 24 นิ้ว','ยางในจักรยานแบบธรรมดา', 100.00, 1);
+values (2, 'HDD 1TB','Hard disk 1 TB', 100.00, 1);
 
 
 insert into product (id, name, description, price, category_id)
-values (4, 'กะทะ','อย่างดี Non-stick', 690.00, 2);
+values (3, 'CPU Intel Core i7','CPU Intel Core i7', 100.00, 1);
+
+
+insert into product (id, name, description, price, category_id)
+values (4, 'Model Figure','Spider Man Model Figure', 690.00, 2);
 
